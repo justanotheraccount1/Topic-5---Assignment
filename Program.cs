d4d661790d7d966ce5283a9d9bcd774105f4a215
339729139 <339729139@gotvdsb.ca>
2025-09-22 09:25:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Topic_5___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string compass;
+            double degree;
+
+            Console.Write("Please enter a bearing between 0 and 360 degrees:");
+            while (!Double.TryParse(Console.ReadLine(), out degree) || degree < 0 ||  degree > 360)
+            {
+                Console.Write("That is an invalid input... Try Again:");
+            }
+            Console.WriteLine();
+            if (degree >= 315 || degree <= 45)
+            {
+                compass = "North";
+            }
+            else if (degree >= 270 && degree <= 225)
+            {
+                compass = "West";
+            }
+            else if (degree <= 135 && degree >= 45)
+            {
+                compass = "East";
+            }
+            else
+            {
+                compass = "South";
+            }
+            Console.WriteLine();
+            Console.WriteLine($"The compass is pointing {compass}");
+
+
+        }
+    }
+}
