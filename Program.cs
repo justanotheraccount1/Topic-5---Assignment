b846c7819740c05ca6713bce35b288cb779419a2
339729139 <339729139@gotvdsb.ca>
2025-09-23 09:34:04 -0400

    pt 3

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
             double degree;
 
             Console.Write("Please enter a bearing between 0 and 360 degrees:");
-            while (!Double.TryParse(Console.ReadLine(), out degree) || degree < 0 ||  degree > 360)
+            while (!Double.TryParse(Console.ReadLine(), out degree) || degree < 0 || degree > 360)
             {
                 Console.Write("That is an invalid input... Try Again:");
             }
@@ -29,8 +29,27 @@
             {
                 compass = "South";
             }
-            Console.WriteLine();
             Console.WriteLine($"The compass is pointing {compass}");
+            Console.WriteLine();
+
+
+            int minutes, cost;
+            cost = 4;
+            Console.Write("Please enter the time spent at the parking garage to the nearest minute:");
+            while(!Int32.TryParse(Console.ReadLine(), out minutes))
+            { 
+                Console.Write("That is an invalid input... Try Again:");
+            } 
+            if (minutes >= 60)
+            {
+                cost = cost + (2 * (minutes / 60));
+            }
+            if (cost > 20)
+            {
+                cost = 20;
+            }
+            Console.WriteLine($"The cost for parking is ${cost}.00");
+
 
 
         }
